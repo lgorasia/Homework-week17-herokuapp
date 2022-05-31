64d45d86f9180929d7107c7dba5e503112c99e7d
lgorasia <lalit.gorasia295@gmail.com>
2022-05-31 23:21:18 +0100

    Homework-week17-herokuapp

---
+++ src/test/java/com/herokuapp/testbase/TestBase.java
@@ -0,0 +1,16 @@
+package com.herokuapp.testbase;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+
+public class TestBase {
+
+    @BeforeClass
+    public static void inIt() {
+        RestAssured.baseURI="https://restful-booker.herokuapp.com";
+
+       // RestAssured.basePath="/api";
+
+    }
+}
